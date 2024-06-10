f4fecc5d5fd6e2fdce2b675f84fe7767ccb920eb
Jan-Marco Haldemann <j.haldemann@proton.me>
2024-06-10 16:58:42 +0000

    feat: working pose orientation constraint

--- ofa_weed_control/src/arm_moveit.cpp
+++ ofa_weed_control/src/arm_moveit.cpp
@@ -1,7 +1,11 @@
 #include <memory>
+#include <cmath>
+#include <chrono>
 
 #include <rclcpp/rclcpp.hpp>
 #include <moveit/move_group_interface/move_group_interface.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_listener.h>
 
 int main(int argc, char* argv[])
 {
@@ -14,89 +18,52 @@ int main(int argc, char* argv[])
   // Create a ROS logger
   auto const logger = rclcpp::get_logger("arm_moveit");
 
+  auto t1 = std::chrono::high_resolution_clock::now();
+
   // Create the MoveIt MoveGroup Interface
   using moveit::planning_interface::MoveGroupInterface;
   auto move_group_interface = MoveGroupInterface(node, "arm");
 
-  // set position goal
-  move_group_interface.setPositionTarget(0.2, 0.1, 0, "eef_link");
+  move_group_interface.setMaxVelocityScalingFactor(1.0);
+  move_group_interface.setMaxAccelerationScalingFactor(1.0);
 
-  // create service client to set parameters
-  rclcpp::Client<rcl_interfaces::srv::SetParametersAtomically>::SharedPtr client =
-    node->create_client<rcl_interfaces::srv::SetParametersAtomically>("/move_group/set_parameters_atomically");
+  // set position goal (left position)
+  geometry_msgs::msg::Pose target_pose;
+  tf2::Quaternion q;
+  q.setRPY(45 * M_PI / 180, 0, 0);
+  // RCLCPP_INFO(logger, "quaternion: %f %f %f %f", q.x(), q.y(), q.z(), q.w());
+  target_pose.orientation.w = q.w();
+  target_pose.orientation.x = q.x();
+  target_pose.orientation.y = q.y();
+  target_pose.orientation.z = q.z();
+  target_pose.position.x = 0.2;
+  target_pose.position.y = 0.1;
+  target_pose.position.z = 0;
+  move_group_interface.setPoseTarget(target_pose);
+  move_group_interface.setGoalOrientationTolerance(45 * M_PI / 180);
+
+  // plan and execute
+  move_group_interface.move();
+
+  // set position goal (right position)
+  q.setRPY(-45 * M_PI / 180, 0, 0);
+  // RCLCPP_INFO(logger, "quaternion: %f %f %f %f", q.x(), q.y(), q.z(), q.w());
+  target_pose.orientation.w = q.w();
+  target_pose.orientation.x = q.x();
+  target_pose.orientation.y = q.y();
+  target_pose.orientation.z = q.z();
+  target_pose.position.x = 0.2;
+  target_pose.position.y = 0.1;
+  target_pose.position.z = 0;
+  move_group_interface.setPoseTarget(target_pose);
+  move_group_interface.setGoalOrientationTolerance(45 * M_PI / 180);
+
+  // plan and execute
+  move_group_interface.move();
   
-  // set joint limits for treating left plant row
-  auto request = std::make_shared<rcl_interfaces::srv::SetParametersAtomically::Request>();
-  rcl_interfaces::msg::Parameter parameter;
-  parameter.name = "robot_description_planning.joint_limits.joint2.max_position";
-  parameter.value.type = 3;
-  parameter.value.double_value = 0;
-  request->parameters.push_back(parameter);
-  parameter.name = "robot_description_planning.joint_limits.joint2.min_position";
-  parameter.value.type = 3;
-  parameter.value.double_value = -2.1;
-  request->parameters.push_back(parameter);
-  auto result = client->async_send_request(request);
-  if (rclcpp::spin_until_future_complete(node, result) ==
-    rclcpp::FutureReturnCode::SUCCESS)
-  {
-    RCLCPP_INFO(logger, "Changed joint limits.");
-  } else {
-    RCLCPP_ERROR(logger, "Failed to change joint limits.");
-  }
-
-  // Create a plan to that target pose
-  auto const [success, plan] = [&move_group_interface] {
-    moveit::planning_interface::MoveGroupInterface::Plan msg;
-    auto const ok = static_cast<bool>(move_group_interface.plan(msg));
-    return std::make_pair(ok, msg);
-  }();
-
-  // Execute the plan
-  if (success)
-  {
-    move_group_interface.execute(plan);
-  }
-  else
-  {
-    RCLCPP_ERROR(logger, "Planning failed!");
-  }
-
-  // // set joint limits for treating right plant row
-  request->parameters.clear();
-  parameter.name = "robot_description_planning.joint_limits.joint2.max_position";
-  parameter.value.type = 3;
-  parameter.value.double_value = 2.1;
-  request->parameters.push_back(parameter);
-  parameter.name = "robot_description_planning.joint_limits.joint2.min_position";
-  parameter.value.type = 3;
-  parameter.value.double_value = 0;
-  request->parameters.push_back(parameter);
-  auto result2 = client->async_send_request(request);
-  if (rclcpp::spin_until_future_complete(node, result2) ==
-    rclcpp::FutureReturnCode::SUCCESS)
-  {
-    RCLCPP_INFO(logger, "Changed joint limits.");
-  } else {
-    RCLCPP_ERROR(logger, "Failed to change joint limits.");
-  }
-
-  // Create a plan to that target pose
-  auto const [success2, plan2] = [&move_group_interface] {
-    moveit::planning_interface::MoveGroupInterface::Plan msg;
-    auto const ok = static_cast<bool>(move_group_interface.plan(msg));
-    return std::make_pair(ok, msg);
-  }();
-
-  // Execute the plan
-  if (success2)
-  {
-    move_group_interface.execute(plan2);
-  }
-  else
-  {
-    RCLCPP_ERROR(logger, "Planning failed!");
-  }
+  auto t2 = std::chrono::high_resolution_clock::now();
+  auto ms_int = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+  RCLCPP_INFO(logger, "%ld ms", ms_int.count());
 
   // Shutdown ROS
   rclcpp::shutdown();
