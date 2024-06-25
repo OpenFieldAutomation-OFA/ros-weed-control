54d8d6e0d0c0563da02079b71f8df9304c21fabe
Jan-Marco Haldemann <146882261+janmacro@users.noreply.github.com>
2024-06-25 16:57:08 +0000

    chore: update cmakelists

--- ofa_weed_detection/src/process_image.cpp
+++ ofa_weed_detection/src/process_image.cpp
@@ -1,10 +1,14 @@
 #include <cstdio>
+#include <k4a/k4a.h>
 
 int main(int argc, char ** argv)
 {
   (void) argc;
   (void) argv;
 
-  printf("hello world ofa_weed_detection package\n");
+  uint32_t count = k4a_device_get_installed_count();
+
+  printf("test: %u", count);
+
   return 0;
 }
