209b106a0739d8794e9dd7da2033dc8e60993490
Jan-Marco Haldemann <j.haldemann@proton.me>
2024-08-26 20:32:40 +0200

    chore: ikfast

--- ofa_ikfast_plugin/src/ofa_robot_arm_ikfast_solver.cpp
+++ ofa_ikfast_plugin/src/ofa_robot_arm_ikfast_solver.cpp
@@ -12,7 +12,7 @@
 /// See the License for the specific language governing permissions and
 /// limitations under the License.
 ///
-/// ikfast version 0x10000049 generated on 2024-08-13 10:03:06.156536
+/// ikfast version 0x10000049 generated on 2024-08-26 18:31:37.380946
 /// To compile with gcc:
 ///     gcc -lstdc++ ik.cpp
 /// To compile without any main function as a shared object (might need -llapack):
